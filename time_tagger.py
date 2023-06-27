59a1f8e84585491f38a65ecd53a75e13e44281da
ays-2 <137795775+ays-2@users.noreply.github.com>
2023-06-27 15:04:08 -0400

    Revert "Yicheng code"

--- time_tagger.py
+++ time_tagger.py
@@ -5,43 +5,36 @@ import math
 from scipy import stats
 import statsmodels.api as sm
 
-# lambd = average count rate (counts/second, typically on the order of 10^4/s)
-lambd = 100000
+# lambd = average count rate
+lambd = 1
 
 # n = the number of events recorded
-n = 1000000
-
-#time interval to integrate counts, say 1ms (1e9ps)
-interval = 1e9
+n = 2000
 
 # interval width for the poisson distribution
-# interval_width = 10
+interval_width = 10
 
-#timestamps in multiples of 1ps, just as the real time taggers are specified
 timestamps = []
 
-#generating pseudo timestamps following an exp distribution
 t = 0
 for i in range(n):
-    t += math.floor(random.expovariate(lambd) * 1e12)
+    t += random.expovariate(lambd)
     timestamps.append(t)
 
+# calculate the number of events occuring in a fixed interval of time
+# the poisson distribution is an appropriate model for counts
+num_intervals = math.floor(timestamps[n - 1]/interval_width)
+counts = np.zeros(num_intervals + 1)
+curr_interval = 0
 
-t_start = timestamps[0]
-t_stop = timestamps[-1]
+for i in range(n - 1):
+    # if the time is larger than the upper bound of the current time interval, move on to the next interval
+    if timestamps[i] > (curr_interval + 1)*interval_width:
+        curr_interval += 1
+    # add to the counts
+    counts[curr_interval] += 1
 
-# Now we want to count how many time tags are there within each 1ms interval
-# this count number is recorded by the counts[] array
-# the size of counts[] is pre-specified to avoid trouble
-counts = np.zeros(math.floor(t_stop / interval) + 1)
-
-# loop through the timestamps to put them into the correct time intervals
-for i in range(n):
-    index = math.floor((timestamps[i] - t_start) / interval)
-    counts[index] +=1
-
-
-plt.hist(counts, bins=20)
+plt.hist(counts)
 plt.xlabel('Number of occurences in the interval')
 plt.ylabel('Frequency')
 plt.show()
