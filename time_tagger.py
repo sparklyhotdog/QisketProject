d9152231491bd51a21bf573fbd783d787d18d8c0
Shi <yns19@NIST.GOV>
2023-06-27 11:57:57 -0400

    code modified; now the counts[] array carries counts integrated over 1ms and follows the poisson distribution

--- time_tagger.py
+++ time_tagger.py
@@ -5,34 +5,48 @@ import math
 from scipy import stats
 import statsmodels.api as sm
 
-# lambd = average count rate
-lambd = 1
+# lambd = average count rate (counts/second, typically on the order of 10^4/s)
+lambd = 100000
 
 # n = the number of events recorded
-n = 2000
+n = 100000
+
+#time interval to integrate counts, say 1ms (1e9ps)
+interval = 1e9
 
 # interval width for the poisson distribution
 interval_width = 10
 
+#timestamps in multiples of 1ps, just as the real time taggers are specified
 timestamps = []
 
 t = 0
 for i in range(n):
-    t += random.expovariate(lambd)
+    t += math.floor(random.expovariate(lambd) * 1e12)
     timestamps.append(t)
+
+
+t_start = timestamps[0]
+t_stop = timestamps[-1]
+
+counts = np.zeros(math.floor(t_stop / interval) + 1)
+
+for i in range(n):
+    index = math.floor((timestamps[i] - t_start) / interval)
+    counts[index] +=1
 
 # calculate the number of events occuring in a fixed interval of time
 # the poisson distribution is an appropriate model for counts
-num_intervals = math.floor(timestamps[n - 1]/interval_width)
-counts = np.zeros(num_intervals + 1)
-curr_interval = 0
+# num_intervals = math.floor(timestamps[n - 1]/interval_width)
+# counts = np.zeros(num_intervals + 1)
+# curr_interval = 0
 
-for i in range(n - 1):
-    # if the time is larger than the upper bound of the current time interval, move on to the next interval
-    if timestamps[i] > (curr_interval + 1)*interval_width:
-        curr_interval += 1
-    # add to the counts
-    counts[curr_interval] += 1
+# for i in range(n - 1):
+#     # if the time is larger than the upper bound of the current time interval, move on to the next interval
+#     if timestamps[i] > (curr_interval + 1)*interval_width:
+#         curr_interval += 1
+#     # add to the counts
+#     counts[curr_interval] += 1
 
 plt.hist(counts)
 plt.xlabel('Number of occurences in the interval')
