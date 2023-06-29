17dc2565a6142bc5067c22d1c326fff737b261bc
ays2 <ays2@nist.gov>
2023-06-29 15:43:32 -0400

    split coincidence_counting.py into time_tagger_run.py to write the timestamps into a txt file and coincidence_counting_run.py

---
+++ timetagging_&_coincidence_counting/coincidence_counting_run.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+n = 1000000                     # total number of events (1 mil)
+coincidence_interval = 10000    # (picoseconds)
+
+timestamps_signal = []          # (picoseconds)
+timestamps_idler = []           # (picoseconds)
+
+# read in the timestamps from the txt files
+with open('timestamps_signal') as s:
+    for line in s:
+        timestamps_signal.append(int(line))
+
+with open('timestamps_idler') as i:
+    for line in i:
+        timestamps_idler.append(int(line))
+
+# count coincidences
+x_val = range(-100000, 100000, 10000)
+y_val = []
+if len(timestamps_signal) > len(timestamps_idler):
+    list2 = timestamps_signal
+    list1 = timestamps_idler
+else:
+    list1 = timestamps_signal
+    list2 = timestamps_idler
+
+for delta_t in x_val:
+    coincidences = []
+    # index in list2 of the left bound
+    left_bound = 0
+    for x in list1:
+        # check interval (x - coincidence_interval, x + coincidence_interval)
+        while list2[left_bound] < (x + delta_t) - coincidence_interval and left_bound < len(list2) - 1:
+            left_bound += 1
+        # now x - coincidence_interval <= larger[left_bound]
+        # ** < or <= **
+        if list2[left_bound] < (x + delta_t) + coincidence_interval:
+            # x and larger[left_bound] are in the same window
+            # the timestamp of the coincidence is the time of the later event
+            coincidences.append(max(x + delta_t, list2[left_bound]))
+    print(len(coincidences)/n)
+    y_val.append(len(coincidences))
+plt.plot(x_val, y_val)
+plt.show()
---
+++ timetagging_&_coincidence_counting/time_tagger_poisson.py
@@ -0,0 +1,45 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from scipy import stats
+import statsmodels.api as sm
+
+# lambd = average count rate (100k counts/second)
+lambd = 100000
+
+# n = the number of events recorded (1 mil)
+n = 1000000
+
+# time interval to integrate counts (1ms = 1e9ps)
+interval_width = 1e9
+
+# (picoseconds)
+timestamps = []
+
+# generating pseudo timestamps following an exponential distribution
+t = 0
+for i in range(n):
+    t += math.floor(random.expovariate(lambd) * 1e12)
+    timestamps.append(t)
+
+# count how many time tags are there within each 1ms interval
+counts = np.zeros(math.floor(timestamps[-1]/interval_width) + 1)
+
+# loop through timestamps and put them in the correct time intervals
+curr_interval = 0
+for i in range(n):
+    if timestamps[i] > (curr_interval + 1)*interval_width:
+        curr_interval += 1
+    counts[curr_interval] += 1
+
+plt.hist(counts, bins=20)
+plt.xlabel('Number of occurences in the interval')
+plt.ylabel('Frequency')
+plt.show()
+
+qdist = stats.poisson(np.mean(counts))
+
+sm.qqplot(counts, dist=qdist, line='45')
+plt.title('Poisson Probability Plot')
+plt.show()
