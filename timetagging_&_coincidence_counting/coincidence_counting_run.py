52e41f8613fe1741e0f4cb6af65bf59b2462a78d
ays2 <ays2@nist.gov>
2023-07-03 12:42:48 -0400

    added a progress bar

--- timetagging_&_coincidence_counting/coincidence_counting_run.py
+++ timetagging_&_coincidence_counting/coincidence_counting_run.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from alive_progress import alive_bar
 
 n = 1000000                     # total number of events (1 mil)
-coincidence_interval = 1000    # (picoseconds)
+coincidence_interval = 10000    # (picoseconds)
 
 timestamps_signal = []          # (picoseconds)
 timestamps_idler = []           # (picoseconds)
@@ -16,7 +17,7 @@ with open('timestamps_idler') as i:
         timestamps_idler.append(int(line))
 
 # count coincidences
-x_val = range(-100000, 100000, 1000)
+x_val = range(-100000, 100000, coincidence_interval)
 y_val = []
 if len(timestamps_signal) > len(timestamps_idler):
     list2 = timestamps_signal
@@ -25,21 +26,23 @@ else:
     list1 = timestamps_signal
     list2 = timestamps_idler
 
-for delta_t in x_val:
-    coincidences = []
-    # index in list2 of the left bound
-    left_bound = 0
-    for x in list1:
-        # check interval (x + delta_t - coincidence_interval, x + delta_t + coincidence_interval)
-        while list2[left_bound] < (x + delta_t) - coincidence_interval and left_bound < len(list2) - 1:
-            left_bound += 1
-        # now x + delta_t - coincidence_interval <= larger[left_bound]
-        if list2[left_bound] < (x + delta_t) + coincidence_interval:
-            # x + delta_t and larger[left_bound] are in the same window
-            # the timestamp of the coincidence is the time of the later event
-            coincidences.append(max(x + delta_t, list2[left_bound]))
-    print(len(coincidences))
-    y_val.append(len(coincidences))
+with alive_bar(len(x_val), force_tty=True) as bar:
+    for delta_t in x_val:
+        coincidences = []
+        # index in list2 of the left bound
+        left_bound = 0
+        for x in list1:
+            # check interval (x + delta_t - coincidence_interval, x + delta_t + coincidence_interval)
+            while list2[left_bound] < (x + delta_t) - coincidence_interval and left_bound < len(list2) - 1:
+                left_bound += 1
+            # now x + delta_t - coincidence_interval <= larger[left_bound]
+            if list2[left_bound] < (x + delta_t) + coincidence_interval:
+                # x + delta_t and larger[left_bound] are in the same window
+                # the timestamp of the coincidence is the time of the later event
+                coincidences.append(max(x + delta_t, list2[left_bound]))
+        # print(len(coincidences))
+        bar(1)
+        y_val.append(len(coincidences))
 plt.plot(x_val, y_val)
 plt.xlabel('Time difference (ps)')
 plt.ylabel('Counts')
