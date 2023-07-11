8c02dd069f265f2ae5b287db2d4f9e35ece14f21
ays2 <ays2@nist.gov>
2023-07-11 15:43:51 -0400

    added counts/second

--- simulations_with_practical_issues/Simulator.py
+++ simulations_with_practical_issues/Simulator.py
@@ -23,17 +23,21 @@ class Simulator:
         self.deadtime = self.dicty['deadtime']
         self.jitter_fwhm = self.dicty['jitter_fwhm']
         self.coincidence_interval = self.dicty['coincidence_interval']
+
         self.timestamps_signal = []
         self.timestamps_idler = []
+
         self.max_counts = 0
-        # for the cross correlation plot
         self.histo = np.array
         self.dtime = np.array
         self.bins = np.array
+        self.accidentals = np.array
+        self.coincidences = np.array
 
     def run(self):
-        n = self.total_time * self.lambd                  # total number of events
         # generate pseudo timestamps following an exponential distribution
+        n = self.total_time * self.lambd                  # total number of events
+
         t = 0
         for i in range(math.floor(n)):
             dt = math.floor(random.expovariate(self.lambd) * 1e12)
@@ -41,9 +45,9 @@ class Simulator:
             if random.random() < self.pr:
                 # optical loss
                 if random.random() > self.optical_loss_signal:
-                    self.timestamps_signal.append(t)
+                    self.timestamps_signal.append(t + self.lag)
                 if random.random() > self.optical_loss_idler:
-                    self.timestamps_idler.append(t + self.lag)
+                    self.timestamps_idler.append(t)
 
         # jitter
         sigma = self.jitter_fwhm / (2 * math.sqrt(2 * math.log(2)))
@@ -80,7 +84,7 @@ class Simulator:
         # ___________________________________________________________________
         # count coincidences
 
-        range_ps = 200000
+        range_ps = 200000           # checks the time difference for (-range_ps/2, range_ps/2)
 
         s_floor = np.int64(np.floor(np.array(self.timestamps_signal) / (range_ps / 2)))
         i_floor = np.int64(np.floor(np.array(self.timestamps_idler) / (range_ps / 2)))
@@ -96,7 +100,6 @@ class Simulator:
         # iterate over coincidence_interval, find max of the max(histo)'s
         num_steps = 2           # the number of dt's checked for the max
         for dt in np.arange(0, self.coincidence_interval, self.coincidence_interval/num_steps):
-
             bins = np.arange(-range_ps / 2 + dt, range_ps / 2 + self.coincidence_interval, self.coincidence_interval)
             histo = np.histogram(self.dtime, bins)[0]
             curr_count = max(histo)
@@ -107,15 +110,21 @@ class Simulator:
             else:
                 break
 
-    def get_coincidences(self):
-        return self.max_counts
+        # ___________________________________________________________________
+        # seperate coincidences and accidentals
 
-    def get_car(self):
-        i = np.argmax(self.histo)
-        x = 1
-        accidentals = np.delete(self.histo, range(i - x, i + x))
-        if np.mean(accidentals) > 0:
-            return self.max_counts/np.mean(accidentals)
+        epsilon = 1             # the difference in means allowed amoung the accidentals
+        max_i = np.argmax(self.histo)
+        i = 0
+        prev = np.mean(np.delete(self.histo, range(max_i - i, max_i + i + 1)))
+        curr = np.mean(np.delete(self.histo, range(max_i - i - 1, max_i + i + 2)))
+        while abs(prev - curr) > epsilon:
+            i += 1
+            prev = np.mean(np.delete(self.histo, range(max_i - i, max_i + i + 1)))
+            curr = np.mean(np.delete(self.histo, range(max_i - i - 1, max_i + i + 2)))
+
+        self.accidentals = np.delete(self.histo, range(max_i - i, max_i + i + 1))
+        self.coincidences = np.split(self.histo, [max_i - i, max_i + i + 1])[1]
 
     def plot_cross_corr(self):
         plt.hist(self.dtime, self.bins)
@@ -123,6 +132,25 @@ class Simulator:
         plt.ylabel('Counts')
         plt.savefig('plots\\cross_correlation_plot.png', dpi=1000, bbox_inches='tight')
         plt.show()
+
+    def get_car(self):
+        if np.mean(self.accidentals) > 0:
+            return self.max_counts/np.mean(self.accidentals)
+
+    def get_coincidences(self):
+        return self.max_counts
+
+    def get_coincidences_per_sec(self):
+        # total collection time for the signal and idler (in seconds)
+        t_signal = (self.timestamps_signal[-1] - self.timestamps_signal[0])/1e12
+        t_idler = (self.timestamps_idler[-1] - self.timestamps_idler[0])/1e12
+        return 2 * sum(self.coincidences) / (t_signal + t_idler)
+
+    def get_accidentals_per_sec(self):
+        # total collection time for the signal and idler (in seconds)
+        t_signal = (self.timestamps_signal[-1] - self.timestamps_signal[0]) / 1e12
+        t_idler = (self.timestamps_idler[-1] - self.timestamps_idler[0]) / 1e12
+        return 2 * sum(self.accidentals) / (t_signal + t_idler)
 
     def set_dc_rate(self, dc):
         self.dark_count_rate = dc
@@ -143,6 +171,8 @@ class Simulator:
 if __name__ == "__main__":
     a = Simulator(pr=1, yaml_fn='config.yaml')
     a.run()
-    print(a.get_coincidences())
-    print(a.get_car())
+    print('Coincidences: ' + str(a.get_coincidences()))
+    print('Coincidence-to-Accidental Ratio: ' + str(a.get_car()))
+    print('Coincidences per second: ' + str(a.get_coincidences_per_sec()))
+    print('Accidentals per second: ' + str(a.get_accidentals_per_sec()))
     a.plot_cross_corr()
